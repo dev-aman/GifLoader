5ca94e4e616c3af47168f0c74ee0e2cef7160f51
dev-aman <aman.gupta@tokopedia.com>
2021-04-10 09:51:34 +0530

    Add spacing

--- GifLoader/ViewController.swift
+++ GifLoader/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        performanceTrackingManager.start()
+        performanceTrackingManager.start() 
         networkCall()
     }
 
